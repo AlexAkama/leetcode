8567f652af6c4ef7033eb7e9f51a64245d8beab0
Alex Akama <gremcox@bk.ru>
2022-07-04 16:40:57 +0500

    added solution 58

---
+++ src/main/java/leetcode/solution/Solution0058v2.java
@@ -0,0 +1,23 @@
+package leetcode.solution;
+
+//
+// Поиск длинны последнего слова
+//
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Length of Last Word.
+//Memory Usage: 41.7 MB, less than 80.43% of Java online submissions for Length of Last Word.
+public class Solution0058v2 {
+
+    public int lengthOfLastWord(String s) {
+        int counter = 0;
+        int i = s.length() - 1;
+        while (i > -1 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i > -1 && s.charAt(i) != ' ') {
+            counter++;
+            i--;
+        }
+        return counter;
+    }
+
+}
