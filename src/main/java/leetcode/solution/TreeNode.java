0623c783c3fd685b59508fa19be3770966a50231
Alex Akama <gremcox@bk.ru>
2022-07-06 20:44:18 +0500

    Added TreeNode

---
+++ src/main/java/leetcode/solution/TreeNode.java
@@ -0,0 +1,22 @@
+package leetcode.solution;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
