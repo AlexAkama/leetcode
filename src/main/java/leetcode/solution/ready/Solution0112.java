ba579fafe2fa86fd280cd5f05e7888e498f12dc5
Alex Akama <gremcox@bk.ru>
2022-07-10 21:13:31 +0500

    Added solution 112

---
+++ src/main/java/leetcode/solution/ready/Solution0112.java
@@ -0,0 +1,28 @@
+package leetcode.solution.ready;
+
+//
+// TreeNode
+// Проверка суммы пути до листа
+//
+// Ход мыслей:
+// 1. Посчитать все - сложить куда - то, а потом проверить есть что надо или нет - как-то очень громоздко получилось.
+// 2. Считаем и возвращаем что-то если не верно и искомую цифру если оно и сравнивать
+// - возникла проблема с тем что возвращать если есть два варианта
+// 3. Возвращать сразу boolean
+//
+//Runtime: 1 ms, faster than 60.07% of Java online submissions for Path Sum.
+//Memory Usage: 44 MB, less than 28.62% of Java online submissions for Path Sum.
+public class Solution0112 {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return checkSum(root, 0, targetSum);
+    }
+
+    private boolean checkSum(TreeNode node, int sum, int targetSum) {
+        if (node == null) return false;
+        sum += node.val;
+        if (node.left == null && node.right == null) return sum == targetSum;
+        return checkSum(node.left, sum, targetSum) || checkSum(node.right, sum, targetSum);
+    }
+
+}
