aadd95fd1fcd8a7d662e5441bb1c2bae2eddbd78
Alex Akama <gremcox@bk.ru>
2022-07-11 21:36:57 +0500

    Added solution 206

---
+++ src/main/java/leetcode/solution/ready/Solution0206.java
@@ -0,0 +1,25 @@
+package leetcode.solution.ready;
+
+//
+// ListNode
+// Разворот односвязного списка
+//
+public class Solution0206 {
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+    //Memory Usage: 43.4 MB, less than 16.11% of Java online submissions for Reverse Linked List.
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode node = head;
+        ListNode next;
+        while (node != null) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return prev;
+    }
+
+}
