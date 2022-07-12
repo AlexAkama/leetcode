74ac290aada2bfa44fed14813d131f806eba0278
Alex Akama <gremcox@bk.ru>
2022-07-12 11:36:22 +0500

    Added solution 142

---
+++ src/main/java/leetcode/solution/ready/Solution0142.java
@@ -0,0 +1,29 @@
+package leetcode.solution.ready;
+
+//
+// Поиск цикла в односвязном списке
+// и возврат узда с которого начинается цикл
+//
+public class Solution0142 {
+
+    //Runtime: 1 ms, faster than 56.31% of Java online submissions for Linked List Cycle II.
+    //Memory Usage: 44.5 MB, less than 81.02% of Java online submissions for Linked List Cycle II.
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (slow != head) {
+                    slow = slow.next;
+                    head = head.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+}
