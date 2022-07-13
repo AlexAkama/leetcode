da2f3af10a07ad8a7f66b21add921a71fc858d32
Alex Akama <gremcox@bk.ru>
2022-07-13 16:31:26 +0500

    Added solution 160

---
+++ src/main/java/leetcode/solution/ready/Solution0160.java
@@ -0,0 +1,41 @@
+package leetcode.solution.ready;
+
+//
+// TreeNode
+// Определить пресекаются ли два односвязных списка, если да то вернуть узел пересечения
+//
+// Проходим оба списка до конца, если последний одинаковый, то пересекаются.
+// Если пересекаются зацикливаем на одну из голов, и со второй ищем пересечение (Заяц и Черепаха)
+public class Solution0160 {
+
+    //Runtime: 2 ms, faster than 56.17% of Java online submissions for Intersection of Two Linked Lists.
+    //Memory Usage: 56 MB, less than 9.57% of Java online submissions for Intersection of Two Linked Lists.
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1.next != null || p2.next != null) {
+            if (p1.next != null) p1 = p1.next;
+            if (p2.next != null) p2 = p2.next;
+        }
+        if (p1 != p2) return null;
+        ListNode last = p1;
+        last.next = headB;
+        p1 = headA;
+        p2 = headA;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2) {
+                ListNode circleHead = headA;
+                while (p1 != circleHead) {
+                    p1 = p1.next;
+                    circleHead = circleHead.next;
+                }
+                last.next = null;
+                return circleHead;
+            }
+        }
+        return null;
+    }
+
+}
