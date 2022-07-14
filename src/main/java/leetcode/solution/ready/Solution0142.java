01f429ffe5bcb28dddc162de74b2af93b2012792
Alex Akama <gremcox@bk.ru>
2022-07-14 11:17:30 +0500

    Updated result info

--- src/main/java/leetcode/solution/ready/Solution0142.java
+++ src/main/java/leetcode/solution/ready/Solution0142.java
@@ -8,6 +8,9 @@ public class Solution0142 {
 
     //Runtime: 1 ms, faster than 56.31% of Java online submissions for Linked List Cycle II.
     //Memory Usage: 44.5 MB, less than 81.02% of Java online submissions for Linked List Cycle II.
+    //---
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle II.
+    //Memory Usage: 45 MB, less than 52.84% of Java online submissions for Linked List Cycle II.
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) return null;
         ListNode slow = head;
--- src/main/java/leetcode/solution/ready/Solution0876.java
+++ src/main/java/leetcode/solution/ready/Solution0876.java
@@ -7,6 +7,8 @@ package leetcode.solution.ready;
 //
 public class Solution0876 {
 
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Middle of the Linked List.
+    //Memory Usage: 42.1 MB, less than 18.33% of Java online submissions for Middle of the Linked List.
     public ListNode middleNode(ListNode head) {
         ListNode slowPinter = head;
         ListNode fastPointer = head;
