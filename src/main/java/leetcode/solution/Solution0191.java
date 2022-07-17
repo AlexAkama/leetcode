714f002887d5d80e549f4d5b978effe65f4a4d1f
Alex Akama <gremcox@bk.ru>
2022-07-18 00:25:42 +0500

    Added solution 191

---
+++ src/main/java/leetcode/solution/Solution0191.java
@@ -0,0 +1,23 @@
+package leetcode.solution;
+
+//
+// Возвращает кол-во битов "1" в числе (вес Хэмминга)
+//
+// Побитовые операции
+public class Solution0191 {
+
+    //Runtime: 1 ms, faster than 85.79% of Java online submissions for Number of 1 Bits.
+    //Memory Usage: 41.1 MB, less than 61.81% of Java online submissions for Number of 1 Bits.
+    public int hammingWeight(int n) {
+        int res = 0;
+        int i = 32;
+        while (i != 0) {
+            int last = n & 1;
+            n = n >> 1;
+            res += last;
+            i--;
+        }
+        return res;
+    }
+
+}
