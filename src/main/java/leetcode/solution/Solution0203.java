ca40900bddc43ea8c0a0d861a003635960bdbcc4
Alex Akama <gremcox@bk.ru>
2022-07-19 20:14:57 +0500

    Added solution 203

---
+++ src/main/java/leetcode/solution/Solution0203.java
@@ -0,0 +1,45 @@
+package leetcode.solution;
+
+//
+// ListNode
+// Удаление узлов с указанным значением
+//
+public class Solution0203 {
+
+    //Runtime: 1 ms, faster than 98.99% of Java online submissions for Remove Linked List Elements.
+    //Memory Usage: 43.5 MB, less than 84.50% of Java online submissions for Remove Linked List Elements.
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        ListNode resHead = new ListNode(0);
+        ListNode prev = resHead;
+        ListNode node = head;
+        while (node != null) {
+            if (node.val != val) {
+                prev.next = node;
+                prev = node;
+            } else {
+                prev.next = node.next;
+            }
+            node = node.next;
+        }
+        return resHead.next;
+    }
+
+    //Runtime: 1 ms, faster than 98.99% of Java online submissions for Remove Linked List Elements.
+    //Memory Usage: 49.2 MB, less than 34.32% of Java online submissions for Remove Linked List Elements.
+    public ListNode removeElements2(ListNode head, int val) {
+        ListNode node = new ListNode(0);
+        node.next = head;
+        head = node;
+        while (node.next != null) {
+            if (node.next.val == val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head.next;
+    }
+
+
+}
