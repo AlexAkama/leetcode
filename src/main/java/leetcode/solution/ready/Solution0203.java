b244dc4e3d7e71b0d46509899f13c56db91b4ca3
Alex Akama <gremcox@bk.ru>
2022-07-19 21:15:56 +0500

    Moved solution 203

--- src/main/java/leetcode/solution/ready/Solution0203.java
+++ src/main/java/leetcode/solution/ready/Solution0203.java
@@ -1,4 +1,6 @@
-package leetcode.solution;
+package leetcode.solution.ready;
+
+import leetcode.solution.ListNode;
 
 //
 // ListNode
@@ -8,11 +10,11 @@ public class Solution0203 {
 
     //Runtime: 1 ms, faster than 98.99% of Java online submissions for Remove Linked List Elements.
     //Memory Usage: 43.5 MB, less than 84.50% of Java online submissions for Remove Linked List Elements.
-    public ListNode removeElements(ListNode head, int val) {
+    public leetcode.solution.ListNode removeElements(leetcode.solution.ListNode head, int val) {
         if (head == null) return null;
-        ListNode resHead = new ListNode(0);
-        ListNode prev = resHead;
-        ListNode node = head;
+        leetcode.solution.ListNode resHead = new leetcode.solution.ListNode(0);
+        leetcode.solution.ListNode prev = resHead;
+        leetcode.solution.ListNode node = head;
         while (node != null) {
             if (node.val != val) {
                 prev.next = node;
@@ -27,8 +29,8 @@ public class Solution0203 {
 
     //Runtime: 1 ms, faster than 98.99% of Java online submissions for Remove Linked List Elements.
     //Memory Usage: 49.2 MB, less than 34.32% of Java online submissions for Remove Linked List Elements.
-    public ListNode removeElements2(ListNode head, int val) {
-        ListNode node = new ListNode(0);
+    public leetcode.solution.ListNode removeElements2(leetcode.solution.ListNode head, int val) {
+        leetcode.solution.ListNode node = new ListNode(0);
         node.next = head;
         head = node;
         while (node.next != null) {
