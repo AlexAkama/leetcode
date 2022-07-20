ecdc6faf7f07be66a6b2d0c8a4c1453470abd764
Alex Akama <gremcox@bk.ru>
2022-07-20 11:28:01 +0500

    Added solution 237

---
+++ src/main/java/leetcode/solution/ready/Solution0237.java
@@ -0,0 +1,16 @@
+package leetcode.solution.ready;
+
+import leetcode.solution.ListNode;
+
+//
+// ListNode
+// Удаление ноды, но есть только ссылка на нее, т.е. на голову ссылки нет
+//
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+//Memory Usage: 41.8 MB, less than 89.78% of Java online submissions for Delete Node in a Linked List.
+public class Solution0237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
