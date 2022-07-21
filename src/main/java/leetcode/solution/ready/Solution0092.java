dbfd859a5e21ceafcc9590e3256e82fa932c28d1
Alex Akama <gremcox@bk.ru>
2022-07-21 13:06:07 +0500

    Added solution 92

---
+++ src/main/java/leetcode/solution/ready/Solution0092.java
@@ -0,0 +1,58 @@
+package leetcode.solution.ready;
+
+import leetcode.solution.ListNode;
+
+//
+// ListNode
+// Разворот односвязного списка внутри указанных позиций
+//
+// Ход мыслей:
+// Выделить промежуток, которой надо развернуть
+// не забыть определить начало и конец
+// Крайние варианты начало первая нода и/или конец последняя
+public class Solution0092 {
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List II.
+    //Memory Usage: 41.3 MB, less than 77.99% of Java online submissions for Reverse Linked List II.
+    public leetcode.solution.ListNode reverseBetween(leetcode.solution.ListNode head, int left, int right) {
+        if (head.next == null) return head;
+        leetcode.solution.ListNode node = head;
+        leetcode.solution.ListNode start = null;
+        leetcode.solution.ListNode finish = null;
+        leetcode.solution.ListNode prev = null;
+        leetcode.solution.ListNode next = null;
+        int pos = 1;
+        while (node != null) {
+            if (node.next != null && pos + 1 == left) prev = node;
+            if (pos == left) start = node;
+            if (pos == right && start != null) {
+                finish = node;
+                next = node.next;
+                finish.next = null;
+                break;
+            }
+            node = node.next;
+            pos++;
+        }
+        if (start == null || finish == null) return head;
+        start = reverseList(start, next);
+        if (prev == null) head = start;
+        else prev.next = start;
+        return head;
+    }
+
+    public leetcode.solution.ListNode reverseList(leetcode.solution.ListNode head, leetcode.solution.ListNode after) {
+        if (head == null) return null;
+        leetcode.solution.ListNode prev = after;
+        leetcode.solution.ListNode node = head;
+        ListNode next;
+        while (node != null) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return prev;
+    }
+
+}
