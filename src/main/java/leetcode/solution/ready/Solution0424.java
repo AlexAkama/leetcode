1888570ed3ebf7e2bc2c7d7c312922c15ef47118
Alex Akama <gremcox@bk.ru>
2022-08-01 13:34:12 +0500

    Added solution 424

---
+++ src/main/java/leetcode/solution/ready/Solution0424.java
@@ -0,0 +1,22 @@
+package leetcode.solution.ready;
+
+//
+// Window
+// Вернуть длину самой большой подстроки при замене выбранной буквы k раз
+// https://leetcode.com/problems/longest-repeating-character-replacement/
+//
+public class Solution0424 {
+
+    public int characterReplacement(String s, int k) {
+        char[] chars = s.toCharArray();
+        int[] w = new int[26];
+        int max = 0;
+        int p = 0;
+        for (int i = 0; i < s.length(); i++) {
+            max = Math.max(max, ++w[chars[i] - 'A']);
+            if (max + k <= i - p) w[chars[p++] - 'A']--;
+        }
+        return chars.length - p;
+    }
+
+}
