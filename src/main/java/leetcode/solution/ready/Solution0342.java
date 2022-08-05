c4b225d02e9abf665ba46c25c8a9432919b1bfdd
Alex Akama <gremcox@bk.ru>
2022-08-05 09:36:44 +0500

    Updated solution 342

--- src/main/java/leetcode/solution/ready/Solution0342.java
+++ src/main/java/leetcode/solution/ready/Solution0342.java
@@ -16,4 +16,14 @@ public class Solution0342 {
         return n == 1;
     }
 
+    //Runtime: 1 ms, faster than 100.00% of Java online submissions for Power of Four.
+    //Memory Usage: 41.8 MB, less than 18.79% of Java online submissions for Power of Four.
+    public boolean isPowerOfFourRecursive(int n) {
+        if (n == 1) return true;
+        if (n % 4 != 0) return false;
+        if (n == 0) return false;
+        n /= 4;
+        return isPowerOfFourRecursive(n);
+    }
+
 }
