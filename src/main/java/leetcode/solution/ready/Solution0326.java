3ea3e93ea879cc6afd1ae01174c55b319916da28
Alex Akama <gremcox@bk.ru>
2022-08-05 09:37:32 +0500

    Updated solution 326

--- src/main/java/leetcode/solution/ready/Solution0326.java
+++ src/main/java/leetcode/solution/ready/Solution0326.java
@@ -12,12 +12,22 @@ public class Solution0326 {
 
     //Runtime: 23 ms, faster than 55.03% of Java online submissions for Power of Three.
     //Memory Usage: 47.2 MB, less than 61.74% of Java online submissions for Power of Three.
-    public boolean isPowerOfThree1(int n) {
+    public boolean isPowerOfThree(int n) {
         if (n == 0) return false;
         while (n % 3 == 0) {
             n /= 3;
         }
         return n == 1;
+    }
+
+    //Runtime: 19 ms, faster than 80.86% of Java online submissions for Power of Three.
+    //Memory Usage: 47.5 MB, less than 38.43% of Java online submissions for Power of Three.
+    public boolean isPowerOfThreeRecursive(int n) {
+        if (n == 1) return true;
+        if (n % 3 != 0) return false;
+        if (n == 0) return false;
+        n /= 3;
+        return isPowerOfThreeRecursive(n);
     }
 
 }
