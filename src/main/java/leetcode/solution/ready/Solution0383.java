190e374693d2ecf3c3b6fe32b2a8570a2b8c5fb4
Alex Akama <gremcox@bk.ru>
2022-08-25 12:26:46 +0500

    Updated solution 383

--- src/main/java/leetcode/solution/ready/Solution0383.java
+++ src/main/java/leetcode/solution/ready/Solution0383.java
@@ -5,12 +5,27 @@ package leetcode.solution.ready;
 //
 public class Solution0383 {
 
+    //Runtime: 7 ms, faster than 59.10% of Java online submissions for Ransom Note.
+    //Memory Usage: 45.7 MB, less than 71.64% of Java online submissions for Ransom Note.
     public boolean canConstruct(String ransomNote, String magazine) {
         StringBuilder sb = new StringBuilder(magazine);
         for (int i = 0; i < ransomNote.length(); i++) {
             int index = sb.indexOf(Character.toString(ransomNote.charAt(i)));
             if (index == -1) return false;
             sb.deleteCharAt(index);
+        }
+        return true;
+    }
+
+    //Runtime: 2 ms, faster than 99.48% of Java online submissions for Ransom Note.
+    //Memory Usage: 42.4 MB, less than 97.49% of Java online submissions for Ransom Note.
+    public boolean canConstruct1(String ransomNote, String magazine) {
+        int[] w = new int[26];
+        char[] chars = magazine.toCharArray();
+        for (char c : chars) w[c - 'a']++;
+        chars = ransomNote.toCharArray();
+        for (char c : chars) {
+            if (--w[c - 'a'] < 0) return false;
         }
         return true;
     }
