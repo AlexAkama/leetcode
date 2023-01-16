4e65ef4d245d2afcbf4fb395e59a8026b0b5cb27
Alex Akama <gremcox@bk.ru>
2023-01-16 19:23:26 +0500

    Added solution 914

---
+++ src/main/java/leetcode/solution/Solution0914.java
@@ -0,0 +1,51 @@
+package leetcode.solution;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//
+// Разбить на группы одинакового размера
+// https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+//
+// Используется наибольший общий делитель
+//
+public class Solution0914 {
+
+    //Runtime 14 ms. Beats 45.77%
+    //Memory 42.2 MB. Beats 87.68%
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : deck) {
+            map.putIfAbsent(i, 0);
+            map.put(i, map.get(i) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for (Integer value : map.values()) {
+            if (value == 1) return false;
+            set.add(value);
+        }
+        if (set.size() == 1) return true;
+        int prev = 0;
+        int gcd = 10_000;
+        for (Integer integer : set) {
+            if (prev != 0) {
+                int temp = gcd(prev, integer);
+                if (temp < gcd) gcd = temp;
+            }
+            prev = integer;
+        }
+        if (gcd == 1) return false;
+        for (Integer integer : set) {
+            if (integer % gcd != 0) return false;
+        }
+        return true;
+    }
+
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+}
