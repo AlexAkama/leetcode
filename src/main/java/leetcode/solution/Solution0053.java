9c6a96a1a7cd6270689655a8eb6cdf44c1e2903a
Alex Akama <gremcox@bk.ru>
2023-03-15 20:49:00 +0500

    Added solution 53

---
+++ src/main/java/leetcode/solution/Solution0053.java
@@ -0,0 +1,21 @@
+package leetcode.solution;
+
+// Dynamic Programming
+// https://leetcode.com/problems/maximum-subarray/description/
+public class Solution0053 {
+
+    //Runtime 1ms, Beats 100%
+    //Memory 52MB, Beats 22.30%
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int res = nums[0];
+        int sum = Math.max(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+            sum += nums[i];
+            res = Math.max(res, sum);
+            sum = Math.max(sum, 0);
+        }
+        return res;
+    }
+
+}
