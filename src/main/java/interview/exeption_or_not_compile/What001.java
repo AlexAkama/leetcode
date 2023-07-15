5cf18e7e6243d6c6f8cb7e353e9664b7f3cc6355
Alex Akama <gremcox@bk.ru>
2023-07-15 11:38:34 +0500

    Added interview problem
    For by array

---
+++ src/main/java/interview/exeption_or_not_compile/What001.java
@@ -0,0 +1,25 @@
+package interview.exeption_or_not_compile;
+
+//
+// Перебор(цикл) по массиву
+// For by array
+//
+public class What001 {
+
+//    public static void main(String[] args) {
+//        int[] array = {1,2};
+//        int x;
+//        for (x : array) {
+//            System.out.println(x);
+//        }
+//    }
+
+    // Какой результат работы программы
+    // 1) 1 2
+    // 2) 2 1
+    // 3) Ошибка во время компиляции
+    // 4) Ошибка во время выполнения
+
+    // Переменную надо объявить внутри цикла
+
+}
