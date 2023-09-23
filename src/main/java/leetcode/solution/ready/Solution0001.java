8be89671c0c105c18d9be9599651f90d07940569
Alex Akama <gremcox@bk.ru>
2023-09-23 21:05:51 +0500

    Updated solution 1

--- src/main/java/leetcode/solution/ready/Solution0001.java
+++ src/main/java/leetcode/solution/ready/Solution0001.java
@@ -1,12 +1,31 @@
 package leetcode.solution.ready;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Поиск позиций в массве, значения которых в сумме дают целевое значение.
+По условию такая пара всегда существует.
+
+
+ */
 public class Solution0001 {
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSumV0(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) return new int[]{i, j};
             }
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int d = target - nums[i];
+            if (map.containsKey(d)) return new int[]{map.get(d), i};
+            map.put(nums[i], i);
         }
         return new int[0];
     }
